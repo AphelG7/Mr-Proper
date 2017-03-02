8938c173212c257dbe002d7dfe252d4a144bea5c
Luca Friedrich <lucafriedrich@mail.com>
2017-03-02 11:54:24 +0100

    Fixed missing .exe suffix

--- tools/build_listener.sh
+++ tools/build_listener.sh
@@ -10,6 +10,6 @@ GOOS=linux GOARCH=386 go build -o ../../../../../bin/Mr-Proper/MrProper_linux_38
 echo "Building listener for linux amd64..."
 GOOS=linux GOARCH=amd64 go build -o ../../../../../bin/Mr-Proper/MrProper_linux_amd64 github.com/4m4rOk/Mr-Proper/listener
 echo "Building listener for windows i386..."
-GOOS=windows GOARCH=386 go build -o ../../../../../bin/Mr-Proper/MrProper_linux_win32 github.com/4m4rOk/Mr-Proper/listener
+GOOS=windows GOARCH=386 go build -o ../../../../../bin/Mr-Proper/MrProper_linux_win32 github.com/4m4rOk/Mr-Proper/listener.exe
 echo "Building listener for windows amd64..."
-GOOS=windows GOARCH=amd64 go build -o ../../../../../bin/Mr-Proper/MrProper_win64 github.com/4m4rOk/Mr-Proper/listener
+GOOS=windows GOARCH=amd64 go build -o ../../../../../bin/Mr-Proper/MrProper_win64 github.com/4m4rOk/Mr-Proper/listener.exe
