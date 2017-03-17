6b9996609a354ca6593d2df3a889d0de48e38b91
Luca Friedrich <lucafriedrich@mail.com>
2017-03-17 16:43:18 +0100

    Updated file names in the listener building script

--- tools/build_listener.sh
+++ tools/build_listener.sh
@@ -2,14 +2,14 @@ cd "${0%/*}"
 echo "Formatting code..."
 ./fmt_proper.sh
 echo "Building listener for darwin i386..."
-GOOS=darwin GOARCH=386 go build -o ../../../../../bin/Mr-Proper/MrProper_darwin_386 github.com/4m4rOk/Mr-Proper/listener
+GOOS=darwin GOARCH=386 go build -o ../../../../../bin/Mr-Proper/listener/listener_darwin_386 github.com/4m4rOk/Mr-Proper/listener
 echo "Building listener for linux arm..."
-GOOS=linux GOARCH=arm go build -o ../../../../../bin/Mr-Proper/MrProper_linux_arm github.com/4m4rOk/Mr-Proper/listener
+GOOS=linux GOARCH=arm go build -o ../../../../../bin/Mr-Proper/listener/listener_linux_arm github.com/4m4rOk/Mr-Proper/listener
 echo "Building listener for linux i386..."
-GOOS=linux GOARCH=386 go build -o ../../../../../bin/Mr-Proper/MrProper_linux_386 github.com/4m4rOk/Mr-Proper/listener
+GOOS=linux GOARCH=386 go build -o ../../../../../bin/Mr-Proper/listener/listener_linux_386 github.com/4m4rOk/Mr-Proper/listener
 echo "Building listener for linux amd64..."
-GOOS=linux GOARCH=amd64 go build -o ../../../../../bin/Mr-Proper/MrProper_linux_amd64 github.com/4m4rOk/Mr-Proper/listener
+GOOS=linux GOARCH=amd64 go build -o ../../../../../bin/Mr-Proper/listener/listener_linux_amd64 github.com/4m4rOk/Mr-Proper/listener
 echo "Building listener for windows i386..."
-GOOS=windows GOARCH=386 go build -o ../../../../../bin/Mr-Proper/MrProper_win32.exe github.com/4m4rOk/Mr-Proper/listener
+GOOS=windows GOARCH=386 go build -o ../../../../../bin/Mr-Proper/listener/listener_win32.exe github.com/4m4rOk/Mr-Proper/listener
 echo "Building listener for windows amd64..."
-GOOS=windows GOARCH=amd64 go build -o ../../../../../bin/Mr-Proper/MrProper_win64.exe github.com/4m4rOk/Mr-Proper/listener
+GOOS=windows GOARCH=amd64 go build -o ../../../../../bin/Mr-Proper/listener/listener_win64.exe github.com/4m4rOk/Mr-Proper/listener
